f36525ae18e33eb8d1b42b2bb2ed8b1e961d9adb
Clemente Castejon <63917082+ClementeAC@users.noreply.github.com>
2020-10-31 08:41:41 -0400

    Update Registro.java

--- src/servlets/Registro.java
+++ src/servlets/Registro.java
@@ -14,7 +14,7 @@ import helpers.Hashing;
 /**
  * Servlet implementation class Registro
  */
-@WebServlet("https://proyecto1-web2.herokuapp.com/Registro")
+@WebServlet("asyncSupported = false, name = "Registro", urlPatterns = {"/Registro"}")
 public class Registro extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
